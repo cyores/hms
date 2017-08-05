961a992da8370b0526a6efef6d81ca3797e47f76
Christian Yores <christianyores@gmail.com>
2017-08-05 10:28:39 -0400

    Scan for movies button

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -3,7 +3,10 @@
 
 @section('content')
 
-<h1>Movies</h1>
+<div class="flex one two-1200">
+	<div class="four-fifth"><h1>Movies</h1></div>
+	<div class="fifth pad10 padT20"><button class="pull-right" onclick="scan()">Scan for Movies</button></div>
+</div>
 
 <div class="flex one four-1200">
     @foreach($movies as $key => $movie)
@@ -13,6 +16,27 @@
 
 
 <script type="text/javascript">
+
+	function scan() {
+		console.log('Scanning for movies');
+        $.ajax({
+            url: '/movies/scan',
+            type: 'POST',
+            xhr: function() {
+                var mxXhr = $.ajaxSettings.xhr();
+                return mxXhr;
+            },
+            success: function() {
+                console.log('Scan complete');
+                location.reload();
+            },
+            error: function() {
+                console.log('There was an error');
+            },
+            // Options to tell jQuery not to process data or worry about content-type.
+            cache: false, contentType: false, processData: false
+        });
+	}
 
 </script>
 
