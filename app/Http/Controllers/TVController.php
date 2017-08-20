066c88c161569fa4e2f8ddcf7b8b74119bb62511
Christian Yores <christianyores@gmail.com>
2017-08-20 00:28:43 -0400

    TV db init

--- app/Http/Controllers/TVController.php
+++ app/Http/Controllers/TVController.php
@@ -16,8 +16,8 @@ class TVController extends Controller
 {
     public function index() {
     	// $return_array = TV::getAllShows();
-    	// $return_array = TV::scanShows();
-    	$return_array = array();
+    	$return_array = TV::scanShows();
+    	// $return_array = array();
     	return View::make('tv.index', $return_array);
     }
 }
--- app/Models/TV.php
+++ app/Models/TV.php
@@ -12,26 +12,64 @@ use Carbon\Carbon;
 
 class TV extends Model
 {
+	// fix episode file name before you run this
+	// this is init the db tvshows
 	public static function scanShows() {
 		$return_array = array();
 
-		$dir = 'E:/TV';
+		$dir = 'E:/TV/';
 		
 		$get_shows = Utility::getThingsInDir($dir);
 
 		foreach ($get_shows as $key => $show) {
-			$get_sezns = Utility::getThingsInDir($dir . $show);
+			$get_sezns = Utility::getThingsInDir($dir . $show['name']);
 			foreach ($get_sezns as $sezn_num => $sezn) {
-				$get_episodes = Utility::getThingsInDir($dir . $show . $sezn);
+				$get_episodes = Utility::getThingsInDir($dir . $show['name'] . '/' . $sezn['name']);
 				foreach ($get_episodes as $epis_num => $epis) {
-					$s = DB::select('SELECT `id` FROM `tvshows` WHERE `show_title` = ? AND `sezn_num` = ? AND `epis_num` = ?', array($show,, $sezn_num, $epis_num));
-					if(empty($s)) {
+					$title = substr($epis['name'], 11); // episode ## 
+
+					$s = DB::select('SELECT `id` FROM `tvshows` WHERE `show_title` = ? AND `sezn_num` = ? AND `epis_num` = ? AND `epis_title` = ?',
+									array($show['name'], $sezn_num + 1, $epis_num + 1, $title));
+
+					// seasons start at 0 for spartacus
+					$sparta = DB::select('SELECT `id` FROM `tvshows` WHERE `show_title` = ? AND `sezn_num` = ? AND `epis_num` = ? AND `epis_title` = ?',
+									array($show['name'], $sezn_num, $epis_num + 1, $title));
+
+					if(empty($s) && empty($sparta)) {
+						$path = $dir . $show['name'] . '/' . $sezn['name'] . '/' . $epis['name'] . '.' . $epis['type'];
+
+						// because Spartacus has season titles and starts at season 0
+						if($show['name'] == 'Spartacus') {
+							switch ($sezn_num) {
+								case 0:
+									$sezn['name'] = 'Gods of the Arena';
+									break;
+								case 1:
+									$sezn['name'] = 'Blood and Sand';
+									break;
+								case 2:
+									$sezn['name'] = 'Vengeance';
+									break;
+								case 3:
+									$sezn['name'] = 'War of the Damned';
+									break;
+								default:
+									break;
+							}
+							$i = DB::insert('INSERT `tvshows` (`show_title`,`sezn_title`,`sezn_num`,`epis_title`,`epis_num`,`path`) VALUES (?,?,?,?,?,?)',
+										array($show['name'], $sezn['name'], $sezn_num, $title, $epis_num + 1, $path));
+							continue;
+						}
+
 						$i = DB::insert('INSERT `tvshows` (`show_title`,`sezn_title`,`sezn_num`,`epis_title`,`epis_num`,`path`) VALUES (?,?,?,?,?,?)',
-										array($show, $sezn, $sezn_num, $epis, $epis_num, $path));
+										array($show['name'], $sezn['name'], $sezn_num + 1, $title, $epis_num + 1, $path));
+
 					}
 					
 				}
 			}
 		}
+
+		return $return_array;
 	}
 }
