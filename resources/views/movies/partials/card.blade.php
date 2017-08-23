c66c3328711eb5fbf88923e0b052cc9071e4b118
Christian Yores <christianyores@gmail.com>
2017-08-23 16:33:04 -0400

    Fixed style on movie cards

--- resources/views/movies/partials/card.blade.php
+++ resources/views/movies/partials/card.blade.php
@@ -1,6 +1,12 @@
 
 <div id="{{ $movie['movie_id'] }} " class="full half-700 fourth-1200 margin0 pad20">
-	<a href="/movies/{{ $movie['movie_id'] }}"><img class="stack" src="images/default-movie.png"></a>
+	<img class="stack" src="images/default-movie.png" onclick="navTo('/movies/{{ $movie['movie_id'] }}')" style="cursor: pointer;">
 	<a href="/movies/{{ $movie['movie_id'] }}" class="button btn-a stack">{{ $movie['title'] }}</a>
 	<a href="/movies/{{ $movie['movie_id'] }}" class="button btn-a stack">Rating: {{ $movie['rating'] }} <span class="pull-right">Views: {{ $movie['count'] }}</span></a>
 </div>
+
+<script type="text/javascript">
+	function navTo(link) {
+		window.location = link;
+	}
+</script>
