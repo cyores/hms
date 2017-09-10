ef50eb02308de10d1ac48845c16f1b7c27d9654e
Christian Yores <christianyores@gmail.com>
2017-09-10 18:09:29 -0400

    TA chart init

--- resources/views/transactions/index.blade.php
+++ resources/views/transactions/index.blade.php
@@ -3,10 +3,21 @@
 
 @section('content')
 
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.bundle.min.js"></script>
+
 <style type="text/css">
     table { width: 100%; }
     td, th { padding: 10px; }
 </style>
+
+<div class="flex one two-1200 pad50">
+    <div class="full half-1200">
+        <canvas id="all-chart"></canvas>
+    </div>
+    <div class="full half-1200">
+        <canvas id="mo-chart"></canvas>
+    </div>
+</div>
 
 <h1>All Transactions</h1>
 
@@ -41,5 +52,38 @@
     </label>
 </div>
 
+<script>
+
+
+var ctx = document.getElementById("all-chart").getContext('2d');
+var allChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            data: [12, 19, 3, 5, 2, 3],
+        }]
+    },
+    options: {
+
+    }
+});
+
+
+var ctxTwo = document.getElementById("mo-chart").getContext('2d');
+var moChart = new Chart(ctxTwo, {
+    type: 'doughnut',
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            data: [12, 19, 3, 5, 2, 3],
+        }]
+    },
+    options: {
+
+    }
+});
+
+</script>
 
 @endsection
